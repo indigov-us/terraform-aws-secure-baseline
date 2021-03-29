f98e99f017de2930546deb83507d67414d3db4f1
Eli Mattson <eli@indigov.us>
2021-03-29 15:55:43 -0700

    make examples/simple work
    to run this cd ./examples/simple
    terraform init
    terraform plan

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -8,12 +8,14 @@ data "aws_caller_identity" "current" {
 }
 
 resource "aws_iam_user" "admin" {
-  name = "admin"
+  name = "cloud_user"
 }
 
-module "secure_baseline" {
-  source = "../../"
 
+module "secure-baseline" {
+  source  = "nozaq/secure-baseline/aws"
+  version = "0.23.1"
+  # insert the 4 required variables here
   audit_log_bucket_name           = var.audit_s3_bucket_name
   aws_account_id                  = data.aws_caller_identity.current.account_id
   region                          = var.region
@@ -26,23 +28,9 @@ module "secure_baseline" {
 
   providers = {
     aws                = aws
-    aws.ap-northeast-1 = aws.ap-northeast-1
-    aws.ap-northeast-2 = aws.ap-northeast-2
-    aws.ap-northeast-3 = aws.ap-northeast-3
-    aws.ap-south-1     = aws.ap-south-1
-    aws.ap-southeast-1 = aws.ap-southeast-1
-    aws.ap-southeast-2 = aws.ap-southeast-2
-    aws.ca-central-1   = aws.ca-central-1
-    aws.eu-central-1   = aws.eu-central-1
-    aws.eu-north-1     = aws.eu-north-1
-    aws.eu-west-1      = aws.eu-west-1
-    aws.eu-west-2      = aws.eu-west-2
-    aws.eu-west-3      = aws.eu-west-3
-    aws.sa-east-1      = aws.sa-east-1
     aws.us-east-1      = aws.us-east-1
     aws.us-east-2      = aws.us-east-2
     aws.us-west-1      = aws.us-west-1
     aws.us-west-2      = aws.us-west-2
   }
 }
-
